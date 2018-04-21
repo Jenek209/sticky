38311dd2dae63cd4d5ea4665c2e805534fb71c9c
evgeniy <evbelyakov@gmail.com>
2018-04-21 18:05:03 +0300

    Кривая реализация функции добавления окон (закрываются с основным, т.к. дочерние)

--- sticky.py
+++ sticky.py
@@ -1,33 +1,38 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QMenu, QApplication
+from PyQt5.QtWidgets import QMainWindow, QMenu, QApplication, QAction
 from PyQt5.QtCore import Qt
+#from PyQt5.QtGui import QKeySequence
+
 import design
 
 class ExampleApp(QMainWindow, design.Ui_Form):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
         self.setupUi(self)
-        self.setCentralWidget(self.textEdit)
-        self.textEdit.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.textEdit.customContextMenuRequested.connect(self.my_context_menu)
 
-    def my_context_menu(self, pos):
+        self.setCentralWidget(self.textEdit)
+
+        self.textEdit.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.textEdit.customContextMenuRequested.connect(self.myContextMenu)
+
+    def myContextMenu(self, pos):
 
         menu = QMenu(self)
 
-        firstAct = menu.addAction("firstAct")
-        secondAct = menu.addAction("secondAct")
-
-        action = menu.exec_(self.sender().mapToGlobal(pos))
+        menu.addAction(QAction(self.tr('+'), self, triggered=self.addSticker))
+        menu.addAction('color')
+        menu.addAction('format')
 
         menu.exec_(self.mapToGlobal(pos))
+
+    def addSticker(self):
+        ExampleApp(self).show()
 
 def main():
     app = QApplication(sys.argv)
     window = ExampleApp()
     window.show()
-
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
