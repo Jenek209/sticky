66007a03205a09487064102d824d0f9f0db2d7a0
evgeniy <evbelyakov@gmail.com>
2018-04-21 20:33:53 +0300

    Добавил возможность настроить шрифт

--- sticky.py
+++ sticky.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QMenu, QApplication, QAction, QColorDialog
+from PyQt5.QtWidgets import QMainWindow, QMenu, QApplication, QAction, QColorDialog, QFontDialog
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QKeySequence, QColor
 
@@ -21,19 +21,25 @@ class ExampleApp(QMainWindow, design.Ui_Form):
         menu = QMenu(self)
 
         menu.addAction(QAction(self.tr('+'), self, triggered=self.addSticker))
-        menu.addAction('color', self.openColorDialog)
-        menu.addAction('format')
+        menu.addAction('color', self.colorDialog)
+        menu.addAction('format', self.fontDialog)
 
         menu.exec_(self.mapToGlobal(pos))
 
     def addSticker(self):
         ExampleApp(self).show()
 
-    def openColorDialog(self):
+    def colorDialog(self):
         color = QColorDialog.getColor()
 
         if color.isValid():
             self.textEdit.setStyleSheet("QTextEdit {background-color:%s}}" % color.name())
+
+    def fontDialog(self):
+        font, ok = QFontDialog.getFont()
+
+        if ok:
+            self.textEdit.setFont(font)
 
 
 def main():
