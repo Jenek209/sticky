1e61862ea231fde9064598ca5f1ea7ad4a406d49
evgeniy <evbelyakov@gmail.com>
2018-04-22 10:22:53 +0300

    При изменении цвета фона цвет текста меняется на инвертированный цвет фона Добавил возможность изменить цвет текста

--- sticky.py
+++ sticky.py
@@ -2,8 +2,8 @@ import sys
 from PyQt5.QtWidgets import QMainWindow, QMenu, QApplication, QAction, QColorDialog, QFontDialog
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QKeySequence, QColor
-
 import design
+
 
 class ExampleApp(QMainWindow, design.Ui_Form):
     def __init__(self, parent=None):
@@ -16,24 +16,48 @@ class ExampleApp(QMainWindow, design.Ui_Form):
         self.textEdit.setContextMenuPolicy(Qt.CustomContextMenu)
         self.textEdit.customContextMenuRequested.connect(self.myContextMenu)
 
+        self.bcolor = QColor()
+        self.tcolor = QColor()
+
+
     def myContextMenu(self, pos):
 
         menu = QMenu(self)
 
         menu.addAction(QAction(self.tr('+'), self, triggered=self.addSticker))
-        menu.addAction('color', self.colorDialog)
-        menu.addAction('format', self.fontDialog)
+        menu.addAction('background color', self.backgroundColorDialog)
+        menu.addAction('text color', self.textColorDialog)
+        menu.addAction('text format', self.fontDialog)
 
         menu.exec_(self.mapToGlobal(pos))
 
     def addSticker(self):
         ExampleApp(self).show()
 
-    def colorDialog(self):
-        color = QColorDialog.getColor()
+    def backgroundColorDialog(self):
+        self.bcolor = QColorDialog.getColor()
 
-        if color.isValid():
-            self.textEdit.setStyleSheet("QTextEdit {background-color:%s}}" % color.name())
+        if self.bcolor.isValid():
+            self.tcolor = QColor()
+            self.tcolor.setRgb(*[255-x for x in self.bcolor.getRgb()[0:3]], alpha=self.bcolor.getRgb()[3])
+
+            self.textEdit.setStyleSheet("""
+            QTextEdit {{
+                background-color:{0};
+                color:{1};
+            }}
+            """.format(self.bcolor.name(), self.tcolor.name()))
+
+    def textColorDialog(self):
+        self.tcolor = QColorDialog.getColor()
+
+        if self.tcolor.isValid():
+            self.textEdit.setStyleSheet("""
+            QTextEdit {{
+                background-color:{0};
+                color:{1};
+            }}
+            """.format(self.bcolor.name(), self.tcolor.name()))
 
     def fontDialog(self):
         font, ok = QFontDialog.getFont()
@@ -47,6 +71,7 @@ def main():
     window = ExampleApp()
     window.show()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
