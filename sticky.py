130082170c4b81b340cbbf6a4d8f16bc66d56dae
evgeniy <evbelyakov@gmail.com>
2018-04-30 10:12:17 +0300

    Новые окна самостоятельны

--- sticky.py
+++ sticky.py
@@ -7,7 +7,6 @@ import design
 
 
 class StickyApp(QMainWindow, design.Ui_Form):
-
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
@@ -17,33 +16,35 @@ class StickyApp(QMainWindow, design.Ui_Form):
         self.setCentralWidget(self.textEdit)
         self.textEdit.setContextMenuPolicy(Qt.CustomContextMenu)
         self.textEdit.customContextMenuRequested.connect(self.myContextMenu)
-        self.textEdit.textChanged.connect(self.saveText)
+        #self.textEdit.textChanged.connect(self.saveText)
 
         self.bcolor = QColor('#4c4c4c')
         self.tcolor = QColor('#bdbdbd')
 
         self.numb = 0
 
-
     def keyPressEvent(self, event):
-        if ((event.key() == Qt.Key_W) and (event.modifiers() and Qt.ControlModifier)):
+        if (event.key() == Qt.Key_W) and (event.modifiers() and Qt.ControlModifier):
             self.close()
 
-        if ((event.key() == Qt.Key_Q) and (event.modifiers() and Qt.ControlModifier)):
+        if (event.key() == Qt.Key_Q) and (event.modifiers() and Qt.ControlModifier):
             QApplication.instance().quit()
 
-        if ((event.key() == Qt.Key_T) and (event.modifiers() and Qt.ControlModifier)):
+        if (event.key() == Qt.Key_T) and (event.modifiers() == Qt.ControlModifier):
             self.addSticker()
 
-        if ((event.key() == Qt.Key_B) and (event.modifiers() and Qt.ControlModifier)):
+        if (event.key() == Qt.Key_B) and (event.modifiers() and Qt.ControlModifier):
             self.backgroundColorDialog()
 
-        if ((event.key() == Qt.Key_R) and (event.modifiers() and Qt.ControlModifier)):
+        if (event.key() == Qt.Key_R) and (event.modifiers() and Qt.ControlModifier):
             self.textColorDialog()
 
-        if ((event.key() == Qt.Key_O) and (event.modifiers() and Qt.ControlModifier)):
+        if (event.key() == Qt.Key_O) and (event.modifiers() and Qt.ControlModifier):
             self.fontDialog()
 
+        #if (event.key() == Qt.Key_T) and (event.modifiers() and Qt.ControlModifier) \
+        #        and (event.modifiers() and Qt.ShiftModifier):
+        #    self.loadText()
 
     def myContextMenu(self, pos):
         menu = QMenu(self)
@@ -55,26 +56,27 @@ class StickyApp(QMainWindow, design.Ui_Form):
 
         menu.exec_(self.mapToGlobal(pos))
 
-
     def addSticker(self):
-        StickyApp(self).show()
+        addSticker()
 
 
     def backgroundColorDialog(self):
         self.bcolor = QColorDialog.getColor()
         if self.bcolor.isValid():
-            self.tcolor.setRgb(*[255-x for x in self.bcolor.getRgb()[0:3]], alpha=self.bcolor.getRgb()[3])
+            self.tcolor.setRgb(*[255 - x for x in self.bcolor.getRgb()[0:3]],
+                               int_alpha=self.bcolor.getRgb()[3])
             self.setTextStyleSheet()
-
+        else:
+            self.bcolor = QColor('#4c4c4c')
 
     def textColorDialog(self):
         self.tcolor = QColorDialog.getColor()
         if self.tcolor.isValid():
             self.setTextStyleSheet()
-
+        else:
+            self.tcolor = QColor('#bdbdbd')
 
     def setTextStyleSheet(self):
-        print(self.bcolor.name())
         self.textEdit.setStyleSheet("""
         QTextEdit {{
             background-color:{0};
@@ -82,26 +84,31 @@ class StickyApp(QMainWindow, design.Ui_Form):
         }}
         """.format(self.bcolor.name(), self.tcolor.name()))
 
-
     def fontDialog(self):
-        font, ok = QFontDialog.getFont()
+        font, ok = QFontDialog.getFont(self.textEdit)
         if ok:
             self.textEdit.setFont(font)
 
 
-    def saveText(self):
-        with open('{}.dat'.format(self.numb), 'w') as f:
-            f.write(self.textEdit.toPlainText())
+windows = []
+
+
+def addSticker():
+    print('I\'m here')
+    windows.append(StickyApp())
+    windows[-1].show()
+    print(windows[-1])
 
 
 def main():
     import sys
 
     app = QApplication(sys.argv)
-    window = StickyApp()
-    window.show()
+    addSticker()
     sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
     main()
+
+del windows
