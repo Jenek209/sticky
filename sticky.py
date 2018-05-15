d816f1816f8438e0219777d5cdeaea919fbdb0ad
evgeniy <evbelyakov@gmail.com>
2018-05-15 22:25:07 +0300

    Добавил сохранение в sqlite

--- sticky.py
+++ sticky.py
@@ -2,26 +2,50 @@ from PyQt5.QtWidgets import (QMainWindow, QMenu,
                              QApplication, QColorDialog,
                              QFontDialog)
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QColor, QIcon, QPalette
+from PyQt5.QtGui import QIcon, QPalette
+from PyQt5 import QtSql
 import design
 
 
-class Sticky(QApplication):
+class StickyApp(QApplication):
     def __init__(self, args):
         super().__init__(args)
         global qApp
         qApp = self
+        self.createDB()
         self.windows = []
-        self.addSticker(properties=None)
+        self.addSticker(properties={})
 
     def addSticker(self, properties):
+        # Получение последнего id из базы данных
+        self.query.exec_("select max(id) from sticky")
+        self.query.next()
+        # Если он там есть (иначе 0)
+        lastid = self.query.value(0) if self.query.value(0) != '' else 0
+        properties['id'] = 1 + len(self.windows) + lastid
         self.windows.append(StickyWindow(properties=properties))
         self.windows[-1].show()
 
+    def createDB(self):
+        self.db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+        self.db.setDatabaseName('sticky.db')
+        self.db.open()
+        self.query = QtSql.QSqlQuery()
+        self.query.exec_("create table sticky(id int primary key, "
+                    "size varchar(20), font varchar(20), styleSheet text, text text)")
+
+    def save(self, properties):
+        insert = "insert into sticky values({id}, '{font}', '{size}', '{styleSheet}', '{text}')".format(**properties)
+        self.query.exec_(insert)
+        print(insert)
+
+    def load(self):
+        print('I\'m in self.load()')
+
 
 class StickyWindow(QMainWindow, design.Ui_Form):
-    def __init__(self, parent=None, properties=None):
-        super().__init__(parent)
+    def __init__(self, properties={}):
+        super().__init__()
         self.setupUi(self)
         # Добавление иконки
         self.setWindowIcon(QIcon('Sticky.png'))
@@ -32,11 +56,16 @@ class StickyWindow(QMainWindow, design.Ui_Form):
         # Системный цвет фона и текста
         self.bcolor = self.textEdit.palette().color(QPalette.Base)
         self.tcolor = self.textEdit.palette().color(QPalette.WindowText)
+        # Определяются параметры
+        self.properties = properties
         # Наследуется размер, шрифт и цвет
-        if properties is not None:
-            self.setProperties(properties)
-
+        self.setProperties(properties)
+        # Сохраняются изменения текста
+        self.textEdit.textChanged.connect(self.save)
+        # Словарь горячих клавиш
         self.hotkeys = {
+            (Qt.Key_S, int(Qt.ControlModifier)): self.save,
+            (Qt.Key_L, int(Qt.ControlModifier)): self.load,
             (Qt.Key_W, int(Qt.ControlModifier)): self.close,
             (Qt.Key_Q, int(Qt.ControlModifier)): QApplication.instance().quit,
             (Qt.Key_T, int(Qt.ControlModifier)): self.addSticker,
@@ -56,16 +85,14 @@ class StickyWindow(QMainWindow, design.Ui_Form):
         menu.addAction('&background color', self.backgroundColorDialog, 'Ctrl+B')
         menu.addAction('text colo&r', self.textColorDialog, 'Ctrl+R')
         menu.addAction('text f&ormat', self.fontDialog, 'Ctrl+O')
+        menu.addAction('&save', self.save, 'Ctrl+S')
+        menu.addAction('&load', self.load, 'Ctrl+L')
 
         menu.exec_(self.mapToGlobal(pos))
 
     def addSticker(self):
-        properties = {
-            'size': self.textEdit.property('size'),
-            'font': self.textEdit.property('font'),
-            'styleSheet': self.textEdit.property('styleSheet')
-        }
-        qApp.addSticker(properties)
+        self.properties['size'] = self.textEdit.property('size')
+        qApp.addSticker(self.properties)
 
     def backgroundColorDialog(self):
         self.bcolor = QColorDialog.getColor()
@@ -84,31 +111,41 @@ class StickyWindow(QMainWindow, design.Ui_Form):
             self.tcolor = self.textEdit.palette().color(QPalette.WindowText)
 
     def setTextStyleSheet(self):
-        self.textEdit.setStyleSheet("""
+        newStyleSheet = """
         QTextEdit {{
             background-color:{0};
             color:{1};
         }}
-        """.format(self.bcolor.name(), self.tcolor.name()))
+        """.format(self.bcolor.name(), self.tcolor.name())
+        self.textEdit.setStyleSheet(newStyleSheet)
+        self.properties['styleSheet'] = newStyleSheet
 
     def fontDialog(self):
         font, ok = QFontDialog.getFont(self.textEdit)
         if ok:
             self.textEdit.setFont(font)
+            self.properties['font'] = font
 
     def setProperties(self, properties):
-        self.resize(properties['size'])
-        self.textEdit.setFont(properties['font'])
-        self.textEdit.setStyleSheet(properties['styleSheet'])
+        self.resize(properties.get('size', self.textEdit.property('size')))
+        self.textEdit.setFont(properties.get('font', self.textEdit.property('font')))
+        self.textEdit.setStyleSheet(properties.get('styleSheet'))
+        self.properties['font'] = self.textEdit.property('font')
+        self.properties['styleSheet'] = self.textEdit.property('styleSheet')
+
+    def save(self):
+        self.properties['size'] = self.textEdit.property('size')
+        self.properties['text'] = self.textEdit.toPlainText()
+        qApp.save(self.properties)
 
     def load(self):
-        print('I\'m in self.load()')
+        qApp.load()
 
 
 def main():
     import sys
 
-    app = Sticky(sys.argv)
+    app = StickyApp(sys.argv)
     sys.exit(app.exec_())
 
 
